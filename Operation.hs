497d0dfd146108140ad387658356889787938fb5
iemxblog <iemxblog@gmail.com>
2015-06-11 22:38:22 +0200

    Deleted useless comment

--- Operation.hs
+++ Operation.hs
@@ -24,10 +24,6 @@ feed p f = \sp -> [Move (sp+p) (LinearInterpolation f)]
 (+++) :: Operation -> Operation -> Operation
 o1 +++ o2 = \sp -> o1 sp ++ o2 sp
 
--- Operation a = Floating b => V3 b -> a
--- ou 
--- Operation = Floating b => V3 b -> [IR]
-
 test = do
 	o1 <- rapid (V3 1 2 3)
 	o2 <- feed (V3 4 5 6) 100.0
