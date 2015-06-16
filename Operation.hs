02e1065c3d197c0709b2a294a43a8e891721329c
iemxblog <iemxblog@gmail.com>
2015-06-16 21:24:32 +0200

    Added repetitions + pause in Operations.hs

--- Operation.hs
+++ Operation.hs
@@ -96,8 +96,9 @@ oplist :: Double -> [Operation] -> Operation
 oplist _ [] = noOp
 oplist z_safe (o:os) = next z_safe o (oplist z_safe os)
 
+
 runOperation :: Operation -> Program
-runOperation o = evalState (runReaderT o (V3 10 10 10, 100, 30))  (V3 0 0 0)
+runOperation o = evalState (runReaderT o (V3 0 0 0, 100, 30))  (V3 0 0 0)
 
 square = approach (V3 0 0 0) 
 	+++ feed (V3 1 0 0)
@@ -105,3 +106,20 @@ square = approach (V3 0 0 0)
 	+++ feed (V3 0 1 0)
 	+++ feed (V3 0 0 0)
 
+
+repetition :: [V3 Double] -> Double -> Operation -> Operation
+repetition xs z_safe op = foldr (+++) noOp $ map (\v -> retract z_safe +++ translate v op) xs
+
+circularRepetition :: Double -> Int -> Double -> Double -> Operation -> Operation
+circularRepetition d n start_angle z_safe op = repetition pos_list z_safe op
+	where
+		pos_list = [V3 (d/2*cos(f i)) (d/2*sin(f i)) 0 | i <- [0..n-1]]
+		f a = fromIntegral(a)*2*pi/(fromIntegral n)+start_angle*pi/180
+
+gridRepetition :: Int -> Int -> Double -> Double -> Double -> Operation -> Operation
+gridRepetition nx ny space_x space_y z_safe op = repetition pos_list z_safe op
+	where
+		pos_list = [V3 ((fromIntegral x)*space_x) ((fromIntegral y)*space_y) 0 | x <- [1..nx-1], y <- [1..ny-1]]
+
+pause :: Operation
+pause = return [Pause]
