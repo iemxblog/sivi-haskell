24c28e153ffa93efc6ed10a4b2d3a5b44c38831a
iemxblog <iemxblog@gmail.com>
2015-06-20 17:11:11 +0200

    Changed the Operation type to a parameterized type

--- Operation/Base.hs
+++ Operation/Base.hs
@@ -1,5 +1,6 @@
 module Operation.Base (
 	Operation
+	, getOrigin
 	, noOp	
 	, rapid
 	, feed
@@ -29,10 +30,17 @@ import Control.Monad.Reader
 -- * Double : Plunge rate
 --
 -- * Double (in the State Monad) : the current machine position
-type Operation = ReaderT (V3 Double, Double, Double) (State (V3 Double)) [IR]
+type Operation a = ReaderT (V3 Double, Double, Double) (State (V3 Double)) a
+
+
+--getOrigin :: Reader (V3 Double, Double, Double) (V3 Double)
+getOrigin :: Operation (V3 Double)
+getOrigin = do 
+		(or, _, _) <- ask
+		return or
 
 -- | Do-nothing operation
-noOp :: Operation
+noOp :: Operation Program
 noOp = lift $ return []
 
 
@@ -43,16 +51,16 @@ noOp = lift $ return []
 -- pr : plunge rate
 
 -- | Rapid positioning
-rapid :: V3 Double	-- ^ dst : Destination
-	 -> Operation	-- Resulting operation
+rapid :: V3 Double		-- ^ dst : Destination
+	 -> Operation Program	-- Resulting operation
 rapid dst = do
-		(or, fr, pr) <- ask
+		or <- getOrigin
 		lift $ do 
 			put (or+dst)
 			return [Move (or+dst) Rapid]
 -- | Linear interpolation (with the default feedrate)
-feed :: V3 Double	-- ^ dst : Destination
-	 -> Operation	-- Resulting operation
+feed :: V3 Double		-- ^ dst : Destination
+	 -> Operation Program	-- Resulting operation
 feed dst = do
 		(or, fr, pr) <- ask
 		lift $ do
@@ -60,8 +68,8 @@ feed dst = do
 			return [Move (or+dst) (LinearInterpolation fr)]
 
 -- | Linear interpolation (with the plunge feedrate)
-plunge :: V3 Double	-- ^ dst : Destination
-	 -> Operation	-- ^ Resulting operation
+plunge :: V3 Double		-- ^ dst : Destination
+	 -> Operation Program	-- ^ Resulting operation
 plunge dst = do
 		(or, fr, pr) <- ask
 		lift $ do
@@ -69,8 +77,8 @@ plunge dst = do
 			return [Move (or+dst) (LinearInterpolation pr)]
 
 -- | Tool retraction
-retract :: Double 	-- ^ z_safe : destination on the Z axis
-	-> Operation	-- ^ Resulting operation
+retract :: Double 		-- ^ z_safe : destination on the Z axis
+	-> Operation Program	-- ^ Resulting operation
 retract z_safe = do 
 			(or, fr, pr) <- ask
 			lift $ do
@@ -80,8 +88,8 @@ retract z_safe = do
 				return [Move (V3 x y (zo+z_safe)) Rapid]
 
 -- | Rapid in the XY plane (helper function for approach)
-rapid_xy :: V3 Double	-- ^ dst : Destination
-	 -> Operation	-- ^ Resulting operation
+rapid_xy :: V3 Double		-- ^ dst : Destination
+	 -> Operation Program	-- ^ Resulting operation
 rapid_xy dst = do
 			(or, fr, pr) <- ask
 			lift $ do
@@ -91,27 +99,27 @@ rapid_xy dst = do
 				put $ V3 (xo+xd) (yo+yd) z
 				return [Move (V3 (xo+xd) (yo+yd) z) Rapid]
 -- | Rapid in the xy plane + plunge to destination
-approach :: V3 Double	-- ^ dst : Destination
-	 -> Operation	-- ^ Resulting operation
+approach :: V3 Double		-- ^ dst : Destination
+	 -> Operation Program	-- ^ Resulting operation
 approach dst = rapid_xy dst +++ plunge dst
 
 -- | Translate an operation
-translate :: V3 Double	-- ^ v : Translation vector
-	-> Operation 	-- ^ o : Operation to translate
-	-> Operation	-- Resulting operation
+translate :: V3 Double		-- ^ v : Translation vector
+	-> Operation Program	-- ^ o : Operation to translate
+	-> Operation Program	-- Resulting operation
 translate v o =	local (\(or, fr, pr)->(or+v, fr, pr)) o
 
 -- | Chain two operations (without tool retraction between operations)
-(+++) :: Operation 	-- ^ o1 : Operation 1
-	-> Operation	-- ^ o2 : Operation 2
-	-> Operation	-- ^ Operation 1 followed by operation 2
+(+++) :: Operation Program	-- ^ o1 : Operation 1
+	-> Operation Program	-- ^ o2 : Operation 2
+	-> Operation Program	-- ^ Operation 1 followed by operation 2
 o1 +++ o2 = do 
 		ir1 <- o1 
 		ir2 <- o2
 		return $ ir1 ++ ir2
 
 -- | Pause operation, takes no arguments
-pause :: Operation
+pause :: Operation Program
 pause = return [Pause]
 
 -- | Runs an operation with default starting position, feed rate and plunge rate.
@@ -121,7 +129,7 @@ pause = return [Pause]
 -- 	* Feed rate : 100mm/min
 --
 -- 	* Plunge rate : 30 mm/min
-runOperation :: Operation 	-- ^ o : The operation to run
-		-> Program	-- ^ The resulting program in Intermediate Representation
+runOperation :: Operation Program	-- ^ o : The operation to run
+		-> Program		-- ^ The resulting program in Intermediate Representation
 runOperation o = evalState (runReaderT o (V3 0 0 0, 100, 30))  (V3 0 0 0)
 
