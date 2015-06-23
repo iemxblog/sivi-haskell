8b93d1f3e23cf123754d3bf9919512eae0f5537f
iemxblog <iemxblog@gmail.com>
2015-06-23 11:57:49 +0200

    Added zRepetition

--- Operation/Repetition.hs
+++ Operation/Repetition.hs
@@ -1,6 +1,8 @@
 module Operation.Repetition (
 	circularRepetition
 	, gridRepetition
+	, zRepetition
+	, zRange 
 ) where
 
 import Operation.Base
@@ -16,6 +18,9 @@ next z_safe o1 o2 = retract z_safe +++ o1 +++ retract z_safe +++ o2
 
 repetition :: [V3 Double] -> Double -> Operation IR -> Operation IR
 repetition xs z_safe op = foldr (+++) noOp $ map (\v -> retract z_safe +++ translate v op) xs
+
+repetitionWithoutRetract :: [V3 Double] -> Operation IR -> Operation IR
+repetitionWithoutRetract xs op = foldr (+++) noOp $ map (\v -> translate v op) xs
 
 -- | Repeats an operation on a circle (and adds a tool retraction each time)
 circularRepetition :: Double			-- ^ d : Diameter 
@@ -40,3 +45,24 @@ gridRepetition :: Int			-- ^ nx : Number of repetitions on X axis
 gridRepetition nx ny space_x space_y z_safe op = repetition pos_list z_safe op
 	where
 		pos_list = [V3 ((fromIntegral x)*space_x) ((fromIntegral y)*space_y) 0 | x <- [1..nx-1], y <- [1..ny-1]]
+
+-- | Repeats an operation on the z axis (possible to add a tool retraction between each pass)
+zRepetition :: Double			-- ^ depth : Depth of the final pass (relative to the first)
+		-> Double		-- ^ step : Depth of cut	
+		-> Maybe Double		-- ^ m_z_safe : Adds tool retraction to z_safe between each pass (if provided)
+		-> Operation IR		-- ^ op : Operation to repeat
+		-> Operation IR		-- ^ Resulting operation
+zRepetition depth step m_z_safe op = case m_z_safe of
+					Just z_safe -> repetition pos_list z_safe op
+					Nothing -> repetitionWithoutRetract pos_list op
+					where
+						pos_list = map (V3 0 0) $ zRange 0 depth step
+
+-- | Helper function for zRepetition.
+-- Generates a list of depths (on the Z axis) for each iteration of the repetition.
+-- It is close to a haskell range like [0,step..depth], but the last element is equal to depth.
+-- Which is not the case for the range syntax.
+zRange :: (Eq a, Num a) => a -> a -> a -> [a]
+zRange acc depth step 	| signum (depth - acc) == signum step = acc : zRange (acc+step) depth step 
+			| otherwise = [depth]
+
