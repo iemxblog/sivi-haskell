9e86609e12e2647cd318fc5941c528faa168c962
iemxblog <iemxblog@gmail.com>
2015-07-21 15:50:57 +0200

    Added a sivi-rotate utility to rotate GCode files

---
+++ sivi-rotate.hs
@@ -0,0 +1,10 @@
+import IR
+import GCode
+import qualified Data.ByteString as B
+
+main :: IO ()
+main = do
+		s <- B.readFile "test.ngc"
+		case parse s of
+			Left err -> error $ "Parse error : " ++ err
+			Right gcode -> putStrLn . toString . rotateIR (-90) . fromGCode $ gcode
