be72a18cbcd6ab5a1f18397a8ea79a909e68fe0a
iemxblog <iemxblog@gmail.com>
2015-08-07 15:20:49 +0200

    Use of Control.Concurrent.Chan in interface

--- src/Sivi/Interface.hs
+++ src/Sivi/Interface.hs
@@ -16,6 +16,8 @@ import System.Console.ANSI
 import System.IO
 import Control.Concurrent
 import System.Hardware.Serialport
+import qualified Data.ByteString.Char8 as B
+import Control.Monad(forever)
 
 data Input = XMinus | XPlus | YMinus | YPlus | ZMinus | ZPlus | Exit deriving (Eq, Show)
 
@@ -57,13 +59,35 @@ counter n = do
 	threadDelay (10^6)
 	counter (n+1)
 
+serialReader :: Chan String  -> SerialPort -> IO () 
+serialReader chan serial = forever $ do
+	msg <- recv serial 100
+	let msgString = B.unpack msg
+	if length msgString > 0 then
+		do 
+			writeChan chan msgString
+			putStrLn $ "Received : " ++ msgString
+	else
+		return ()
+	 
+
+serialWriter :: Chan String -> SerialPort -> IO ()
+serialWriter chan serial = forever $ do
+	msg <- readChan chan
+	send serial (B.pack msg)
+	putStrLn $ "Sent : " ++ msg
+	
 interface :: IO ()
 interface = do
 	let port = "/dev/ttyACM0"
 	s <- openSerial port defaultSerialSettings { commSpeed = CS115200 }
 	initInterface 
 	putStrLn "Serial port opened."
+	chanRead <- newChan
+	chanWrite <- newChan
 	forkIO $ counter 0
+	forkIO $ serialReader chanRead s
+	forkIO $ serialWriter chanWrite s
 	loop s
 	closeSerial s
 	exitInterface
