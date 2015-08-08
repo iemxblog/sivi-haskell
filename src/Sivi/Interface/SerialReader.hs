2024201a02d45035e91ea9b348ea4ea774e8518a
iemxblog <iemxblog@gmail.com>
2015-08-08 23:54:16 +0200

    Added parsers in Sivi.Interface.SerialReader

--- src/Sivi/Interface/SerialReader.hs
+++ src/Sivi/Interface/SerialReader.hs
@@ -7,6 +7,7 @@ Maintainer	: iemxblog@gmail.com
 Stability	: experimental
 Portability	: POSIX
 -}
+{-# LANGUAGE OverloadedStrings #-}
 module Sivi.Interface.SerialReader
 (
 	serialReader
@@ -16,8 +17,37 @@ module Sivi.Interface.SerialReader
 import Control.Concurrent.Chan
 import System.Hardware.Serialport
 import qualified Data.ByteString.Char8 as B
+import Data.Attoparsec.ByteString.Char8
+import Control.Applicative
 
 data ReadCommand = Position (Double, Double, Double) | Ok deriving (Eq, Show)
+
+-- ##########################################################################
+-- Parsers
+pPosition :: Parser ReadCommand
+pPosition = do
+	string ">Idle,MPos:"
+	double
+	char ','
+	double
+	char ','
+	double
+	string ",WPos:"
+	x <- double
+	char ','
+	y <- double
+	char ','
+	z <- double
+	char '>'
+	return $ Position (x, y, z)
+
+pOk :: Parser ReadCommand
+pOk = string "ok" >> return Ok
+
+pReadCommand :: Parser ReadCommand
+pReadCommand = pPosition <|> pOk
+
+-- ##########################################################################
 
 -- | Takes a string as parameter, and returns a tuple of the first line and the remaining of the string.
 splitLine :: 	String
@@ -26,15 +56,15 @@ splitLine xs | not ('\n' `elem` xs) = ([], xs)
 splitLine xs = mapSnd tail . break (=='\n') $ xs
 	where mapSnd f (a, b) = (a, f b)
 
-serialReader :: Chan String -> SerialPort -> String -> IO () 
+serialReader :: Chan ReadCommand -> SerialPort -> String -> IO () 
 serialReader chan serial buffer = do
 	msg <- recv serial 100
 	let msgString = B.unpack msg
 	let (newMsg, newBuf) = splitLine (buffer ++ msgString)
 	if length newMsg > 0 then
-		do 
-			writeChan chan newMsg
-			putStrLn $ "Received : " ++ newMsg
+		case parseOnly pReadCommand (B.pack newMsg) of
+			Left err -> return ()
+			Right rc -> writeChan chan rc >> putStrLn ("Received : " ++ show rc)
 	else
 		return ()
 	serialReader chan serial newBuf -- looping recursively
