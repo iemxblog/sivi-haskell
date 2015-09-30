2342df86bf41dcf7eb64c85491146bb51b8557df
iemxblog <iemxblog@gmail.com>
2015-09-30 12:00:32 +0200

    Used the new position tracking in 'toGCode'. All tests passed :)

--- src/Sivi/IR/ToGCode.hs
+++ src/Sivi/IR/ToGCode.hs
@@ -16,29 +16,31 @@ module Sivi.IR.ToGCode
 import Linear
 import Numeric
 import Sivi.IR.Base
+import Sivi.IR.PositionTracking
 import Sivi.GCode 
 
 -- | Helper function for 'toGCode'
-toGCode' :: IR		-- ^ The program to compile 
-	-> V3 Double 	-- ^ The current position of the tool (needed for arcs, because i j k are relative to current position)
-	-> [GCode]	-- ^ The resulting 'GCode'
-toGCode' [] _ = []
-toGCode' (Move (V3 x y z) Rapid : xs) _ = G00 (Just x) (Just y) (Just z) : toGCode' xs (V3 x y z)
-toGCode' (Move (V3 x y z) (LinearInterpolation f) : xs) _ = G01 (Just x) (Just y) (Just z) (Just f) : toGCode' xs (V3 x y z)
-toGCode' (Move (V3 x y z) (Arc dir center f) : xs) cp = g dir (Just x) (Just y) (Just z) (notZero i) (notZero j) (notZero k) (Just f) : toGCode' xs (V3 x y z)
-							where 	g CW = G02
-								g CCW = G03
-								V3 i j k = center - cp
-								notZero v = if v /= 0 then Just v else Nothing
-toGCode' (Move (V3 x y z) (Probe f) : xs) _ = G38d2 (Just x) (Just y) (Just z) (Just f) : toGCode' xs (V3 x y z)
-toGCode' (Sivi.IR.Base.Comment s : xs) cp = Sivi.GCode.GComment s : toGCode' xs cp
-toGCode' (Pause : xs) cp = M00 : toGCode' xs cp
-toGCode' (DefCurPos (V3 x y z) : xs) _ = G92 (Just x) (Just y) (Just z) : toGCode' xs (V3 x y z)
+toGCode' :: Instruction			-- ^ The instruction to compile 
+	-> TrackedPosition GCode	-- ^ The resulting 'GCode' instruction
+toGCode' (Move (V3 x y z) Rapid) = return $ G00 (Just x) (Just y) (Just z)
+toGCode' (Move (V3 x y z) (LinearInterpolation f)) = return $ G01 (Just x) (Just y) (Just z) (Just f)
+toGCode' (Move (V3 x y z) (Arc dir center f)) = do
+							cp <- getPosition
+							let V3 i j k = center - cp
+							let notZero v = if v /= 0 then Just v else Nothing
+							return $ g dir (Just x) (Just y) (Just z) (notZero i) (notZero j) (notZero k) (Just f)
+						where
+							g CW = G02
+							g CCW = G03
+toGCode' (Move (V3 x y z) (Probe f)) = return $ G38d2 (Just x) (Just y) (Just z) (Just f)
+toGCode' (Sivi.IR.Base.Comment s) = return $ Sivi.GCode.GComment s
+toGCode' Pause = return M00
+toGCode' (DefCurPos (V3 x y z)) = return $ G92 (Just x) (Just y) (Just z)
 
 -- | Compiles intermediate representation to 'GCode'
 toGCode :: IR 		-- ^ The program in intermediate representation
 	 -> [GCode] 	-- ^ The generated G-Code
-toGCode p = toGCode' p (V3 0 0 0)
+toGCode = mapTrackPosition toGCode'
 
 -- | Compiles 'IR' (intermediate representation) to a GCode string
 toString :: 	IR 		-- ^ The program in intermediate representation
