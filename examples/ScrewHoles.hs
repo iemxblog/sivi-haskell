3400fddbb5270ba1d5a91a91058b0f25889b6bd3
iemxblog <iemxblog@gmail.com>
2015-10-02 12:12:52 +0200

    Little correction after merge

--- examples/ScrewHoles.hs
+++ examples/ScrewHoles.hs
@@ -5,7 +5,7 @@ module ScrewHoles (
 import Sivi
 import Linear
 
-screwHoles :: Operation IR
+screwHoles :: Operation IRTree
 screwHoles = circularRepetition (28*2) 2 0 1 $
 		circularPocket 5 (10+0.2) 0.5
 		+++ translate (V3 0 0 (-5)) (circularPocket 5 (5+0.5) 0.5) 
