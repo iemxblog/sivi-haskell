c80cb960d65d20dbb274da75d199e9b932dc30ef
iemxblog <iemxblog@gmail.com>
2015-10-02 12:06:33 +0200

    Modified z_plate example (changed the orientation)

--- examples/z_plate.hs
+++ examples/z_plate.hs
@@ -7,7 +7,8 @@ import Sivi
 import Linear hiding (rotate)
 import ScrewHoles
 
-corner = V3 (-20) 5 0
+corner = V3 (-40) 5 0
+probeCorner = translate corner $ probeInnerCornerNE 5 (ProbeTool 3 42)
 
 bearing = chain 1 [
 		circularPocket 14 2.5 0.5
@@ -16,58 +17,44 @@ bearing = chain 1 [
 		, translate (V3 0 0 (-2.5)) $ circularPocket 6.5 (11-2.5) 0.5
 	]
 
-makeProbePocket = translate (V3 (-25) 0 0) $ rectangularPocket 10 10 10 0.5
+makeProbePocket = translate (V3 (-45) 0 0) $ rectangularPocket 10 10 (10+0.5) 0.5
 
-makeAlignmentPocket = translate (V3 70 0 0) $ rectangularPocket 10 10 10 0.5
+makeAlignmentPocket = translate (V3 45 0 0) $ rectangularPocket 10 10 (10+0.5) 0.5
 
 back = chain 1 [ 
-		comment "Place the tool above the location of the bearing and start the spindle"
-		, pause
+		message "Place the tool above the location of the bearing and start the spindle"
 		, makeProbePocket
 		, makeAlignmentPocket
-		, comment "Stop the spindle"
-		, pause
+		, message "Stop the spindle"
 		, translate corner $ probeInnerCornerNE 5 (ProbeTool 3 42)
-		, comment "Start the spindle"
-		, pause
+		, message "Start the spindle"
 		, bearing
-	]
-
-half = chain 1 [
-		translate (V3 9 (-28.6) 0) $ drill 10 1
-		, translate (V3 54.2 (-19.8) 0) $ drill 10 1
-		, zRepetition 10.5 (Just 1) $ contour [V2 (-14.5) 3, V2 (-14.5) (-35), V2 22.75 (-35), V2 62.9 (-24.38), V2 62.9 3] RightSide False
 	]
 
 front1 = chain 1 [
-		translate corner $ probeInnerCornerNE 5 (ProbeTool 3 42)
-		, half
-	]	
-
-front2 = chain 1 [
-		translate corner $ probeInnerCornerNE 5 (ProbeTool 3 42)
+		probeCorner
 		, bearing
-		, rotate (-90) screwHoles
+		, screwHoles
+		, translate (V3 28.6 9 0) $ drill (10+0.5) 1
+		, translate (V3 (-28.6) 9 0) $ drill (10+0.5) 1
+		, zRepetition 10.5 (Just 1) $ contour [V2 (-35) 22.75, V2 (-35) (-14.5), V2 35 (-14.5), V2 35 22.75] RightSide False
 	]
 
-front3 = chain 1 [
-		translate corner $ probeInnerCornerNE 5 (ProbeTool 3 42)
-		, symmetryX half
+front2 = chain 1 [
+		probeCorner
+		, translate (V3 19.8 54.2 0) $ drill (10+0.5) 1
+		, translate (V3 (-19.8) 54.2 0) $ drill (10+0.5) 1
+		, zRepetition 10.5 (Just 1) $ contour [V2 35 22.75, V2 24.38 62.9, V2 (-24.38) 62.9, V2 (-35) 22.75] RightSide False
 	]
 
 zPlate = chain 1 [
-	comment "Begin with the back side of the plate"
-	, pause
+	message "Begin with the back side of the plate"
 	, back
-	, comment "Place the front side face up"
-	, pause
+	, message "Place the front side face up, centered on the lower area of the part"
 	, front1
-	, comment "Place the part to machine the second part of the front side"
+	, message "Place the part to machine the upper area of the front side"
 	, pause
 	, front2
-	, comment "Place the part to machine the third part of the front side"
-	, pause
-	, front3
 	]
 
 main :: IO ()
