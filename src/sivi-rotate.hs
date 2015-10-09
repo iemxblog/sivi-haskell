342f4455b949ca97c0d18572535aab78c7618389
iemxblog <iemxblog@gmail.com>
2015-10-09 16:55:55 +0200

    Corrected a little bug in sivi-rotate

--- src/sivi-rotate.hs
+++ src/sivi-rotate.hs
@@ -5,4 +5,4 @@ main = do
 		s <- readFile "test.ngc"
 		case parseGCode s of
 			Left err -> error err
-			Right gcode -> putStrLn . toString . flatten . runOperationWithDefaultParams . rotate (-90) . fromGCode $ gcode
+			Right gcode -> putStr . toString . flatten . runOperationWithDefaultParams . rotate (-90) . fromGCode $ gcode
