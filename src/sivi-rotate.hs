7bf0691dd2f78f6ededcab45ae656e01bf990ade
iemxblog <iemxblog@gmail.com>
2015-10-10 11:48:47 +0200

    Added argument handling in sivi-rotate (+ input on stdin)

--- src/sivi-rotate.hs
+++ src/sivi-rotate.hs
@@ -1,8 +1,18 @@
 import Sivi
+import System.Environment
+import System.Exit
+
+parse ["-h"] = usage >> exitWith ExitSuccess
+parse [] = usage >> exitWith (ExitFailure 1)
+parse [s] = return (read s :: Double)
+
+rotateProgram angle = do
+	s <- getContents
+	case parseGCode s of
+		Left err -> error err
+		Right gcode -> putStr . toString . flatten . runOperationWithDefaultParams . rotate angle . fromGCode $ gcode
 
 main :: IO ()
-main = do
-		s <- readFile "test.ngc"
-		case parseGCode s of
-			Left err -> error err
-			Right gcode -> putStr . toString . flatten . runOperationWithDefaultParams . rotate (-90) . fromGCode $ gcode
+main = getArgs >>= parse >>= rotateProgram
+
+usage = getProgName >>= return . ("Usage: "++) . (++" [-h] angle") >>= putStrLn
