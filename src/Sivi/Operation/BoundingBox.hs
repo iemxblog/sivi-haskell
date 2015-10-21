cc05ff38c48252c6f3c8dfeb30802cd2115ffa48
iemxblog <iemxblog@gmail.com>
2015-10-21 21:31:59 +0200

    Implemented BoundingBox, and the |>| operator, but problem with types !!!

--- src/Sivi/Operation/BoundingBox.hs
+++ src/Sivi/Operation/BoundingBox.hs
@@ -10,16 +10,28 @@ Portability	: POSIX
 module Sivi.Operation.BoundingBox(
 	Boundary(..)
 	, BoundingBox(..)	
+	, (|>|)
 ) where
 
 import Data.Monoid
+import Linear
+import Control.Monad
+import Sivi.Backend
+import Sivi.Operation.Types
+import Sivi.Operation.Base
+import Sivi.Misc.ArcInterpolation
 
-newtype Boundary = Boundary (Double, Double) deriving (Eq, Show)
+data Boundary = Boundary (Double, Double) 
+		| EmptyBoundary
+		deriving (Eq, Show)
+
 
 newtype BoundingBox = BoundingBox [Boundary] deriving (Eq, Show)
 
 instance Monoid Boundary where
-	mempty = Boundary (0, 0)
+	mempty = EmptyBoundary
+	EmptyBoundary `mappend` b = b
+	b `mappend` EmptyBoundary = b
 	Boundary (m1, p1) `mappend` Boundary (m2, p2) = Boundary (min m1 m2, max p1 p2)
 
 instance Monoid BoundingBox where
@@ -29,16 +41,36 @@ instance Monoid BoundingBox where
 instance Backend BoundingBox where
 	bRapid (V3 x y z) = do
 		td <- getToolDiameter	
-		return $ BoundingBox [Boundary (x-td/2, x+td/2), Boundary (y-td/2, y+td/2), (z, z)]
+		return $ BoundingBox [Boundary (x-td/2, x+td/2), Boundary (y-td/2, y+td/2), Boundary (z, z)]
 	bFeed _ (V3 x y z) = do
 		td <- getToolDiameter	
-		return $ BoundingBox [Boundary (x-td/2, x+td/2), Boundary (y-td/2, y+td/2), (z, z)]
-	bArc _ dir center dst = do
+		return $ BoundingBox [Boundary (x-td/2, x+td/2), Boundary (y-td/2, y+td/2), Boundary (z, z)]
+	bArc f dir center dst = do
+		cp <- getCurrentPosition
+		let points = arcInterpolation cp dst center dir 1
+		liftM mconcat $ mapM (bFeed f) points
+
 		
-	bPause = mempty
+	bPause = return mempty
 
-	bDefCurPos =
+	bProbe _ _ = return mempty
+
+	bDefCurPos _ = return mempty
 	
-	bComment =
+	bComment _ = return mempty
 
-	bName = 
+	bName _ op = op
+
+
+--(|>|) :: Backend a => Operation a -> Operation a -> Operation a
+op1 |>| op2 = do
+	BoundingBox [bx1, _, _] <- op1
+	BoundingBox [bx2, _, _] <- op2
+	case (bx1, bx2) of
+		(EmptyBoundary, _) -> op1 +++ op2
+		(Boundary (_, xp1), EmptyBoundary) -> op1 +++ translate (V3 xp1 0 0) op2
+		(Boundary (_, xp1), Boundary (xm2, _)) -> op1 +++ translate (V3 (xp1-xm2) 0 0) op2
+
+--boundingBox :: Backend a => Operation a -> Operation BoundingBox
+--boundingBox op = 
+-- correct bName in plotter.hs !!!!!!
--- tests/Sivi/Operation/BoundingBoxSpec.hs
+++ tests/Sivi/Operation/BoundingBoxSpec.hs
@@ -10,11 +10,11 @@ import Sivi
 spec :: SpecWith ()
 spec = describe "boundingBox" $ do
 	it "calculates the bounding box of a circular pocket" $ 
-		boundingBox' (circularPocket 20 5 0.5) `shouldBe` BoundingBox [Boundary (-10, 10), Boundary (-10, 10), Boundary (-5, 0)]
+		boundingBox (circularPocket 20 5 0.5) `shouldBe` BoundingBox [Boundary (-10, 10), Boundary (-10, 10), Boundary (-5, 0)]
 	it "calculates the bounding box of a translated circular pocket" $ 
-		boundingBox' (translate (V3 15 20 (-5)) (circularPocket 20 5 0.5)) `shouldBe` BoundingBox [Boundary (5, 25), Boundary (10, 30), Boundary (-10, -5)]
+		boundingBox (translate (V3 15 20 (-5)) (circularPocket 20 5 0.5)) `shouldBe` BoundingBox [Boundary (5, 25), Boundary (10, 30), Boundary (-10, -5)]
 	it "calculates the bounding box of a square" $ 
-		boundingBox' (square 30) `shouldBe` BoundingBox [Boundary (-16.5, 16.5), Boundary (-16.5, 16.5), Boundary (0, 0)]
+		boundingBox (square 30) `shouldBe` BoundingBox [Boundary (-1.5, 31.5), Boundary (-1.5, 31.5), Boundary (0, 0)]
 
 	where
-		boundingBox' op = runOperationWithDefaultParams $ boundingBox op
+		boundingBox = runOperationWithDefaultParams 
